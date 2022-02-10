00d67dca992a35f613049ac7ae34cda8b2e24059
王滕辉 <514840279@qq.com>
2022-02-10 17:53:56 +0800

    1

--- mvn sonar.bat
+++ mvn sonar.bat
@@ -1,1 +1,1 @@
-mvn sonar:sonar   -Dsonar.projectKey=chuxue   -Dsonar.host.url=http://192.168.0.103:9006   -Dsonar.login=b7b8468d46adba8b86f6649dad863d783286d336
+mvn sonar:sonar   -Dsonar.projectKey=chuxue   -Dsonar.host.url=http://192.168.0.103:9006   -Dsonar.login=90645ecef9225a8e6dfcce7d9ec04e0a0a8c9290
